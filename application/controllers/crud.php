d3825817ea5dd7497185cec48463140f6c0bb1e6
Umme Hany <bulamist13@gmail.com>
2020-05-18 04:30:03 +0200

    Update crud.php

--- application/controllers/crud.php
+++ application/controllers/crud.php
@@ -1,51 +1,48 @@
 <?php  
- defined('BASEPATH') OR exit('No direct script access allowed');  
- class Crud extends CI_Controller {  
-      //functions  
-      function index(){  
-           $data["title"] = "Codeigniter Project";  
-           $this->load->view('crud_view', $data);  
-      } 
 
+  defined('BASEPATH') OR exit('No direct script access allowed');  
 
-      function fetch_user(){  
-           $this->load->model("crud_model");  
-           $fetch_data = $this->crud_model->make_datatables();  
-          
-           echo json_encode($fetch_data);  
-      }  
-
-
-
+  class Crud extends CI_Controller {  
    
+    function index(){  
+      $data["title"] = "Codeigniter Project";  
+      $this->load->view('crud_view', $data);  
+    } 
 
 
-      public function addItem(){
-     $this->load->model("crud_model");
+    function fetch_user(){  
+      $this->load->model("crud_model");  
+      $fetch_data = $this->crud_model->make_datatables();  
+      echo json_encode($fetch_data);  
+    }  
 
-    $result = $this->crud_model->addItem();
-    $msg['success'] = false;
-    $msg['type'] = 'add';
-    if($result){
-      $msg['success'] = true;
+
+    function addItem(){
+
+      $this->load->model("crud_model");
+
+      $result = $this->crud_model->addItem();
+      $msg['success'] = false;
+      $msg['type'] = 'add';
+
+        if($result){
+          $msg['success'] = true;
+        }
+      echo json_encode($msg);
     }
-    echo json_encode($msg);
-  }
 
-      public function deleteItem(){
-        $this->load->model("crud_model"); 
-    $result = $this->crud_model->deleteItem();
-    $msg['success'] = false;
-    if($result){
-      $msg['success'] = true;
+    function deleteItem(){
+      $this->load->model("crud_model"); 
+
+      $result = $this->crud_model->deleteItem();
+      $msg['success'] = false;
+
+        if($result){
+          $msg['success'] = true;
+        }
+      echo json_encode($msg);
     }
-    echo json_encode($msg);
-  }
-
-
-
   
- }
+  }
 
-
- ?> 
+?> 
