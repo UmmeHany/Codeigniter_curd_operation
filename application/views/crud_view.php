4416d4a4eb1cbad36ad1416283699aa933340238
Umme Hany <bulamist13@gmail.com>
2020-05-18 04:34:08 +0200

    Update crud_view.php

--- application/views/crud_view.php
+++ application/views/crud_view.php
@@ -1,168 +1,136 @@
 <html>  
- <head> 
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-      <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>  
-      <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>            
-      <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   
-   <title><?php echo $title; ?></title>  
+  <head> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>  
+    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>         
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   
+    <title><?php echo $title; ?></title>  
        
-   <style>  
-           body  
-           {  
-                margin:0;  
-                padding:0;  
-                background-color:#f1f1f1;  
-           }  
-           .box  
-           {  
-                width:900px;  
-                padding:20px;  
-                background-color:#fff;  
-                border:1px solid #ccc;  
-                border-radius:5px;  
-                margin-top:10px;  
-           }  
-      </style>  
- </head>  
- <body> 
+    <style>  
+      body{
+        margin:0;  
+        padding:0;  
+        background-color:#f1f1f1;  
+      }  
+      .box{  
+        width:900px;  
+        padding:20px;  
+        background-color:#fff;  
+        border:1px solid #ccc;  
+        border-radius:5px;  
+        margin-top:10px;  
+      }  
 
- 
+    </style>  
+  </head>  
+  <body> 
 
-  <div class="alert alert-success" style="display: none;">
-    
-  </div>
-
-  <button type="button" id="btnAdd"  data-target="#myModal" class="btn btn-info btn-lg">Add</button>  
-                
-
-
-  
-
-      <div class="container box">  
-           <h3 align="center"><?php echo $title; ?></h3><br />  
-           <div class="table-responsive">  
-                <br />  
-                <table  class="table table-bordered table-striped">  
-                      <thead>
-                          <tr>  
-
-                            <td>Item ID</td>
-                           <td>Item Name</td>
-                           <td>Brand Name</td>
-                           <td>Model Name</td>
-                           <td>Date</td>
-                           <td>Delete Action</td>
-                            
-                               
-                              
-                          </tr> 
-                          </thead>
-                          <tbody id="user_data">
+    <div class="alert alert-success" style="display: none;">
       
-    </tbody> 
+    </div>
+
+    <button type="button" id="btnAdd"  data-target="#myModal" class="btn btn-info btn-lg">Add</button>
+
+    <div class="container box">  
+      <h3 align="center"><?php echo $title; ?></h3><br />  
+      <div class="table-responsive">  
+        <br />  
+        <table  class="table table-bordered table-striped">  
+          <thead>
+            <tr>  
+                <td>Item ID</td>
+                <td>Item Name</td>
+                <td>Brand Name</td>
+                <td>Model Name</td>
+                <td>Date</td>
+                <td>Delete Action</td>                                                     
+            </tr> 
+          </thead>
+          <tbody id="user_data">
+      
+          </tbody> 
                       
-                </table>  
-           </div>  
+      </table>  
       </div>  
+    </div>  
 
+    <div id="myModal" class="modal fade"  role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Modal title</h4>
+          </div>
 
-  
-
-
-<div id="myModal" class="modal fade"  role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Modal title</h4>
-      </div>
-
-<div class="modal-body">
-          <form id="myForm" action="" method="post" class="form-horizontal">
-            
-            <div class="form-group">
-              <label for="name" class="label-control col-md-4">Item ID</label>
-              <div class="col-md-8">
-                <input type="text" name="txtItemID" class="form-control">
+          <div class="modal-body">
+            <form id="myForm" action="" method="post" class="form-horizontal">
+                
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Item ID</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtItemID" class="form-control">
+                </div>
               </div>
-            </div>
 
-           <div class="form-group">
-              <label for="name" class="label-control col-md-4">Item Name</label>
-              <div class="col-md-8">
-                <input type="text" name="txtItemName" class="form-control">
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Item Name</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtItemName" class="form-control">
+                </div>
               </div>
-            </div>
 
-            <div class="form-group">
-              <label for="name" class="label-control col-md-4">Brand ID</label>
-              <div class="col-md-8">
-                <input type="text" name="txtBrandID" class="form-control">
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Brand ID</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtBrandID" class="form-control">
+                </div>
               </div>
-            </div>
 
 
-            <div class="form-group">
-              <label for="name" class="label-control col-md-4">Brand Name</label>
-              <div class="col-md-8">
-                <input type="text" name="txtBrandName" class="form-control">
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Brand Name</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtBrandName" class="form-control">
+                </div>
               </div>
-            </div>
 
 
-            <div class="form-group">
-              <label for="name" class="label-control col-md-4">Model ID</label>
-              <div class="col-md-8">
-                <input type="text" name="txtModelID" class="form-control">
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Model ID</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtModelID" class="form-control">
+                </div>
               </div>
-            </div>
 
 
-            <div class="form-group">
-              <label for="name" class="label-control col-md-4">Model Name</label>
-              <div class="col-md-8">
-                <input type="text" name="txtModelName" class="form-control">
+              <div class="form-group">
+                <label for="name" class="label-control col-md-4">Model Name</label>
+                <div class="col-md-8">
+                  <input type="text" name="txtModelName" class="form-control">
+                </div>
               </div>
-            </div>
+                
+            </form>
+          </div>
 
-            
-          </form>
-      </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" id="btnSave" class="btn btn-primary">Save changes</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" id="btnSave" class="btn btn-primary">Save changes</button>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
- 
+  </body>  
+</html>
 
-
-
-
-      </body>  
- </html>
-    
-
-
-
-      
-
-
-
-
- <script >
-   $(function(){
-   // $(document).ready(function(){ 
+<script >
+  $(function(){
     showAll();
-
-    //Add New
     
-   $('#btnAdd').click(function(){
-   // alert('tst');
+    $('#btnAdd').click(function(){
       $('#myModal').modal('show');
       $('#myModal').find('.modal-title').text('Add New Record');
       $('#myForm').attr('action', '<?php echo base_url() ?>crud/addItem');
@@ -170,8 +138,7 @@
 
 
     $('#btnSave').click(function(){
-     // alert('test');
-     var url = $('#myForm').attr('action');
+      var url = $('#myForm').attr('action');
       var data = $('#myForm').serialize();
      
       var item_id = $('input[name=txtItemID]');
@@ -180,13 +147,8 @@
       var model_id = $('input[name=txtModelID]');
       var name = $('input[name=txtItemName]');
       var m_name = $('input[name=txtModelName]');
-
       
-
-
-
-      
-        $.ajax({
+      $.ajax({
           type: 'ajax',
           method: 'post',
           url:url,
@@ -197,9 +159,9 @@
             if(response.success){
               $('#myModal').modal('hide');
               $('#myForm')[0].reset();
-              if(response.type=='add'){
-                var type = 'added'
-              }
+                if(response.type=='add'){
+                  var type = 'added'
+                }
               $('.alert-success').html('Item '+type+' successfully').fadeIn().delay(4000).fadeOut('slow');
               showAll();
             }else{
@@ -209,63 +171,49 @@
           error: function(){
             alert('Could not add data');
           }
-        });
+      });
       
     });
 
-
-
-    
-
-
-     function showAll(){
+    function showAll(){
       $.ajax({
         type: 'ajax',
         url: 'http://localhost/ci_intro/crud/fetch_user',
-        
-        dataType: 'json',
-        success: function(data){
           
+        dataType: 'json',
+        success: function(data){ 
           var html = '';
           var i;
           for(i=0; i<data.length; i++){
 
             html +='<tr>'+
-                  '<td>'+data[i].item_id+'</td>'+
-                  '<td>'+data[i].name+'</td>'+
-                  '<td>'+data[i].b_name+'</td>'+
-                  '<td>'+data[i].m_name+'</td>'+
-                  '<td>'+data[i].date_added+'</td>'+
-
-                  
-                  '<td>'+
-
-
-                    
-                    '<a href="javascript:;"   class="btn btn-danger item-delete" data="'+data[i].item_id+'">Delete</a>'+
-                  '</td>'+
-                  '</tr>';
-           
-
+              '<td>'+data[i].item_id+'</td>'+
+              '<td>'+data[i].name+'</td>'+
+              '<td>'+data[i].b_name+'</td>'+
+              '<td>'+data[i].m_name+'</td>'+
+              '<td>'+data[i].date_added+'</td>'+  
+              '<td>'+  
+              '<a href="javascript:;"   class="btn btn-danger item-delete" data="'+data[i].item_id+'">Delete</a>'+
+              '</td>'+
+              '</tr>';   
           }
           $('#user_data').html(html);
-        },
-        error: function(){
-          alert('Could not get Data from Database');
-        }
+          },
+          error: function(){
+            alert('Could not get Data from Database');
+          }
       });
-    
+      
 
-}
+    }
 
-
-//delete- 
+    //delete
     $('#user_data').on('click', '.item-delete', function(){
       var id = $(this).attr('data');
-      
-      
-      if(confirm("Are you sure you want to delete this?"))  
-           {  
+        
+        
+      if(confirm("Are you sure you want to delete this?")){ 
+              
         $.ajax({
           type: 'ajax',
           method: 'get',
@@ -275,13 +223,10 @@
           dataType: 'json',
           success: function(response){
             if(response.success){
-
-
-              $('.alert-success').html('Item Deleted successfully').fadeIn().delay(4000).fadeOut('slow');
-              
+              $('.alert-success').html('Item Deleted successfully').fadeIn().delay(4000).fadeOut('slow'); 
               showAll();
             }else{
-              alert('Error');
+                alert('Error');
             }
           },
           error: function(){
@@ -289,35 +234,12 @@
           }
         });
       }
-
-      else  
-           {  
-                return false;       
-           }  
-      
+      else{  
+        return false;       
+      }  
+        
     });
+ 
+  });
 
-
-
-    
-
-
-
-
-
-
-
-    
-
-
-
-
-  
-
-  
-});
-
-
-
-
- </script>
+</script>
