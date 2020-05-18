294daee2dd49dbfc09f5bb0eabee6862d7763db9
Umme Hany <bulamist13@gmail.com>
2020-05-18 04:35:02 +0200

    Update main_views.php

--- application/views/main_views.php
+++ application/views/main_views.php
@@ -1,66 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>My view</title>
-</head>
-<body>
-             <h1>welcomr to my viewpage</h1>>
+  <head>
+  	<title>My view</title>
+  </head>
+  <body>
+    <h1>welcomr to my viewpage</h1>>
 
-             <br />
+    <br />
              
-             <br /> 
+    <br /> 
              
-             <br /> 
+    <br /> 
              
 
-<div  class="table-responsive">
-	<table class="table table-bordered">
-		
-       <tr>
-       	<th>ID</th>
-       	<th>Name</th>
+    <div  class="table-responsive">
+    	<table class="table table-bordered">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
 
+    	</table>
 
+    <?php  
 
+      if($fetch_data->num_rows()>0){
 
+      	foreach ($fetch_data->result() as  $row) {
+    ?>
+      	 <tr>
+            <td><?php  echo $row->ID  ?>   </td>
+            <td><?php  echo $row->Name  ?>   </td>
+      		</tr>
 
-       </tr>>
+      <?php
+      		
+      	}
+      }
 
+    ?>
 
-
-	</table>
-
-<?php  
-
-if($fetch_data->num_rows()>0){
-
-	foreach ($fetch_data->result() as  $row) {
-?>
-		<tr>
-
-         <td><?php  echo $row->ID  ?>   </td>
-         <td><?php  echo $row->Name  ?>   </td>
-
-
-		</tr>
-
-<?php
-		
-	}
-}
-
-?>
-
-
-
-
-
-
-</div>
-
-
-
-
-
-</body>
+    </div>
+  </body>
 </html>>
